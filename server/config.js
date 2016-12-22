d1d46ab48ce47e7f4600459c19d1eefdf0900aa3
wilf312 <g.okada.wilf+github@gmail.com>
2016-12-22 19:25:27 +0900

    initial commit

---
+++ server/config.js
@@ -0,0 +1,30 @@
+module.exports = {
+    port: 4321,
+    api: {
+        req: '/apimock/request',
+        res: '/apimock/response',
+    },
+    db: './db.json',
+    backup: './_bk.json',
+    defaultData: {
+        id: "",
+        version: 1,
+        created: 0,
+        updated: 0,
+        request: {
+            body: "",
+            headers: {
+            },
+            method: "GET",
+            params: [
+                {
+                    key: "value"
+                }
+            ]
+        },
+        response: {
+            body: [],
+            httpStatus: 200
+        }
+    }
+}

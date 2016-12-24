bd5d5446b3c0256180efea4fc643a8c12c97fd71
wilf312 <g.okada.wilf+github@gmail.com>
2016-12-24 15:14:35 +0900

    rename serverjs & mock server change module file

---
+++ src/mock.js
@@ -0,0 +1,41 @@
+var express = require('express')
+var fs = require('fs')
+var config = require('./config.js')
+var _ = require('lodash')
+
+
+module.exports = () => {
+  var mockServer = express()
+  var mockPort = config.port+1
+  var db = JSON.parse(fs.readFileSync(config.db))
+  var uri = `http://localhost:${mockPort}`
+
+
+  console.log('\nstart mock server....\n')
+
+  // set response
+  _.forEach(db, function(value, key) {
+    const method = value.request.method.toLowerCase()
+    const apiPath = value.id
+
+    console.log(`request path: ${uri+apiPath}`)
+
+    mockServer[method](apiPath, (req, res) => {
+      // set response headers
+      res.header('Content-Type', 'application/json; charset=utf-8')
+      res.charset = 'utf-8'
+
+      res.json(value.response.body)
+    })
+  })
+
+  mockServer.listen(mockPort)
+
+  console.log(`\nserver running: ${uri}`)
+
+
+}
+
+
+// open browser uri
+// opn(uri)

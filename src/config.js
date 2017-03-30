cf5a600182da71bccec7cc46134b81039f123756
岡田玄哉 <g.okada@trifort.jp>
2017-03-30 12:50:44 +0900

    コードフォーマット " を 'に変更

--- src/config.js
+++ src/config.js
@@ -8,18 +8,18 @@ module.exports = {
     backup: './_bk.json',
     isRunSaveServer: false,
     defaultData: {
-        id: "",
+        id: '',
         version: 1,
         created: 0,
         updated: 0,
         request: {
-            body: "",
+            body: '',
             headers: {
             },
-            method: "GET",
+            method: 'GET',
             params: [
                 {
-                    key: "value"
+                    key: 'value'
                 }
             ]
         },

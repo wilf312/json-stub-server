1f49b01fde64d9be9c73b9f7cde2a547ed3ca38d
岡田玄哉 <g.okada@trifort.jp>
2017-03-30 18:35:45 +0900

    コードフォーマット

--- src/index.js
+++ src/index.js
@@ -1,50 +1,50 @@
-var forever = require('forever-monitor')
+const forever = require('forever-monitor')
 
-var fs = require('fs')
-var util = require('./json-stub-server/util')
+const fs = require('fs')
+const util = require('./json-stub-server/util')
 
-var config = require('./json-stub-server/config.js')
-var dbPath = process.cwd() + '/'+ config.db
-var backupPath = process.cwd() + '/'+ config.backup
+const config = require('./json-stub-server/config.js')
+const dbPath = process.cwd() + '/'+ config.db
+const backupPath = process.cwd() + '/'+ config.backup
 
 function run(...sources) {
 
-  var configParam
+	var configParam
 
-  if (sources.length === 1) {
-    configParam = Object.assign({}, config,  sources[0])
-  }
-  else {
-    configParam = config
-  }
+	if (sources.length === 1) {
+		configParam = Object.assign({}, config,  sources[0])
+	}
+	else {
+		configParam = config
+	}
 
-  var foreverProcess = new (forever.Monitor)(`${__dirname}/json-stub-server/run.js`, {
-    args: [configParam.port, configParam.isRunSaveServer]
-  }).start()
+	const foreverProcess = new (forever.Monitor)(`${__dirname}/json-stub-server/run.js`, {
+		args: [configParam.port, configParam.isRunSaveServer]
+	}).start()
 
-  if (!util.fileCheck(dbPath)) {
-    util.writeJSON(dbPath, [config.defaultData])
-  }
+	if (!util.fileCheck(dbPath)) {
+		util.writeJSON(dbPath, [config.defaultData])
+	}
 
-  if (!util.fileCheck(backupPath)) {
-    util.writeJSON(backupPath, [config.defaultData])
-  }
+	if (!util.fileCheck(backupPath)) {
+		util.writeJSON(backupPath, [config.defaultData])
+	}
 
-  util.readJSON(dbPath)
+	util.readJSON(dbPath)
 
-  watch(dbPath, ()=> {
-      foreverProcess.restart()
-    })
+	watch(dbPath, ()=> {
+			foreverProcess.restart()
+		})
 }
 
 function watch(target, _callback) {
 
-  fs.watch(target, (eventType, filename) => {
-    if (eventType === 'change') {
-      util.readJSON(dbPath)
-      _callback()
-    }
-  })
+	fs.watch(target, (eventType, filename) => {
+		if (eventType === 'change') {
+			util.readJSON(dbPath)
+			_callback()
+		}
+	})
 }
 
 module.exports = run

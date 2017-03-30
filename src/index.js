ca4140674fb1d572cfd87e82c3e8b4d7ee4a6db3
岡田玄哉 <g.okada@trifort.jp>
2017-03-30 16:52:47 +0900

    foreverの箇所一旦nodeのみで対応

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 var forever = require('forever-monitor')
+var spawn = require('child_process').spawn
 
 var fs = require('fs')
 var util = require('./json-stub-server/util')
 
 var config = require('./json-stub-server/config.js')
-var dbPath = process.cwd() + '/'+ config.db
-var backupPath = process.cwd() + '/'+ config.backup
+var cwd = process.cwd()
+var dbPath = `${cwd}/${config.db}`
+var backupPath = `${cwd}/${config.backup}`
 
 function run(...sources) {
 
@@ -18,9 +20,22 @@ function run(...sources) {
     configParam = config
   }
 
-  var foreverProcess = new (forever.Monitor)(`${__dirname}/json-stub-server/run.js`, {
-    args: [configParam.port, configParam.isRunSaveServer]
-  }).start()
+
+  // forever起動
+  var runNode = spawn(
+    'node',
+    [
+      `${__dirname}/json-stub-server/run.js`,
+      configParam.port,
+      configParam.isRunSaveServer,
+    ],
+    {
+      cwd: __dirname,
+      env: process.env
+    });
+  // var foreverProcess = new (forever.Monitor)(`${__dirname}/json-stub-server/run.js`, {
+  //   args: [configParam.port, configParam.isRunSaveServer]
+  // }).start()
 
   if (!util.fileCheck(dbPath)) {
     util.writeJSON(dbPath, [config.defaultData])
@@ -33,8 +48,38 @@ function run(...sources) {
   util.readJSON(dbPath)
 
   watch(dbPath, ()=> {
-      foreverProcess.restart()
-    })
+      // foreverProcess.restart()
+
+      runNode.kill()
+
+      setTimeout(() => {
+        runNode
+      }, 1000)
+  })
+
+  runNode.stdout.on('data', function (data) {
+    console.log('stdout: ' + data)
+  })
+
+  runNode.stderr.on('data', function (data) {
+    console.log('stderr: ' + data)
+  })
+
+  // On 'SIGINT' 
+  process.on('SIGINT', () => {
+
+    runNode.on('close', function (code) {
+      console.log('child process exited with code ' + code);
+    });
+
+    // If the child took too long to exit
+    // Kill the child, and exit with a failure code
+    setTimeout(() => {
+      runNode.kill();
+      process.exit(1);
+    }, 3000);
+
+  });
 }
 
 function watch(target, _callback) {
